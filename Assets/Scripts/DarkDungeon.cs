379dfb568ec5c9fde0251a6cccd3d6de85c85e33
Jenjolen <143611369+Jenjolen@users.noreply.github.com>
2025-03-13 11:25:56 +0100

    added Chaser Level (LevelThree-Chaser)

---
+++ Assets/Scripts/DarkDungeon.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using UnityEngine;
+
+public class DarkDungeonEffect : MonoBehaviour
+{
+    public Camera playerCamera;  // Referencen til spillerens kamera
+    public float normalFOV = 60f;  // Normal synsfelt
+    public float darkDungeonFOV = 40f;  // Begrænset synsfelt i dungeon
+    public float smoothSpeed = 5f;  // Hvor hurtigt FOV ændrer sig
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Dungeon"))  // Tjekker om spilleren er i dungeon-området
+        {
+            StartCoroutine(ChangeFOV(darkDungeonFOV));
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Dungeon"))
+        {
+            StartCoroutine(ChangeFOV(normalFOV));
+        }
+    }
+
+    private IEnumerator ChangeFOV(float targetFOV)
+    {
+        float startFOV = playerCamera.fieldOfView;
+        float timeElapsed = 0f;
+
+        while (timeElapsed < 1f)
+        {
+            playerCamera.fieldOfView = Mathf.Lerp(startFOV, targetFOV, timeElapsed);
+            timeElapsed += Time.deltaTime * smoothSpeed;
+            yield return null;
+        }
+
+        playerCamera.fieldOfView = targetFOV;  // Sæt den ønskede FOV
+    }
+}
